fb08eed6a82499d50daaa5306ac6891044d6c3c6
Vishal kadam <167006989+Vishalkadam2003@users.noreply.github.com>
2026-05-17 22:46:52 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,12 +40,11 @@ Base.metadata.create_all(bind=engine)
 # -------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-    ],
+allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://ai-market-intelligence-phi.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
