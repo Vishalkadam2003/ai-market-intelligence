8e00b4cf67ee9d9f0a148a0a8f0110d054e8afcb
Vishal kadam <167006989+Vishalkadam2003@users.noreply.github.com>
2026-05-17 21:40:48 +0530

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Vishal123%40@localhost:5432/ai_market_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Vishal123%40@localhost:5432/ai_market_db")
 
 engine = create_engine(DATABASE_URL)
 
